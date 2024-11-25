dcf46ec2f189f9b223060feb2354c9a15bcc9d0f
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2024-11-25 10:47:01 +0200

    Add files via upload

---
+++ 07 C# DB Advanced Retake Exam/C# DB Advanced Retake Exam - 11 April 2023/01. Model Definition/Data/Models/Invoice.cs
@@ -0,0 +1,33 @@
+﻿namespace Invoices.Data.Models
+{
+    using Invoices.Data.Models.Enums;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using static Constants.Constants;
+    public class Invoice
+    {
+        //        •	Id – integer, Primary Key
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int Number { get; set; }
+        [Required]
+        public DateTime IssueDate { get; set; }
+
+        [Required]
+        public DateTime DueDate { get; set; }
+        //•	Amount – decimal (required)
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public CurrencyType CurrencyType { get; set; }
+
+        [Required]
+        public int ClientId { get; set; }
+        [ForeignKey(nameof(ClientId))]
+        public Client Client { get; set; } = null!;
+
+    }
+}
