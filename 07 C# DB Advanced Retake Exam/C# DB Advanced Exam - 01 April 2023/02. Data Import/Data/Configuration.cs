6530f8871cc6f3254d6bdcb89183226e5affd603
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2024-11-26 19:07:02 +0200

    Add files via upload

---
+++ 07 C# DB Advanced Retake Exam/C# DB Advanced Exam - 01 April 2023/02. Data Import/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Boardgames.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=localhost\SQLEXPRESS05;Database=Boardgames;Integrated Security=True;Encrypt=False";
+    }
+}
---
+++ 07 C# DB Advanced Retake Exam/C# DB Advanced Exam - 01 April 2023/02. Data Import/Data/Models/Creator.cs
@@ -0,0 +1,26 @@
+﻿namespace Boardgames.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using static Shared.Constants;
+    public class Creator
+    {
+        //•	Id – integer, Primary Key
+        [Key]
+        public int Id { get; set; }
+
+        //•	FirstName – text with length[2, 7] (required) 
+        [Required]
+        [StringLength(CreatorFirstNameMaxLength)]
+        public string FirstName { get; set; } = null!;
+
+        //•	LastName – text with length[2, 7] (required)
+        [Required]
+        [StringLength(CreatorLastNameMaxLength)]
+        public string LastName { get; set; } = null!;
+
+        //•	Boardgames – collection of type Boardgame
+        public HashSet<Boardgame> Boardgames { get; set; } = new HashSet<Boardgame>();
+
+    }
+}
