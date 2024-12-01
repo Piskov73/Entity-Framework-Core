ba42f207b8a45945383f03425c063a98510da192
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2024-12-01 18:58:19 +0200

    Add files via upload

---
+++ 07 C# DB Advanced Retake Exam/Entity Framework Core Retake Exam - 9 August 2024/02. Data Import/DataProcessor/ImportDtos/ExpenseInportDTO.cs
@@ -0,0 +1,42 @@
+﻿namespace NetPay.DataProcessor.ImportDtos
+{
+
+    using System.ComponentModel.DataAnnotations;
+    using NetPay.Data.Models.Enums;
+
+    using static Shared.Constants;
+    public class ExpenseInportDTO
+    {
+        //•	ExpenseName – text with length[5, 50] (required)
+        [Required]
+        [MinLength(ExpenseNameMinLength)]
+        [MaxLength(ExpenseNameMaxLength)]
+        public string ExpenseName { get; set; } = null!;
+
+        //•	Amount - a decimal value in the range[0.01, 100 000](required)
+        [Required]
+        [Range(0.01,100000.00)]
+        public decimal Amount { get; set; }
+
+        //•	DueDate - DateTime(required)
+        [Required]
+        public string DueDate { get; set; }=null!;
+
+        //•	PaymentStatus – PaymentStatus enum (Paid = 1, Unpaid, Overdue, Expired) (required)
+        [Required]
+        [EnumDataType(typeof(PaymentStatus))]
+        public string PaymentStatus { get; set; } = null!;
+
+        //•	HouseholdId - integer, foreign key(required)
+        [Required]
+        public int HouseholdId { get; set; }
+
+
+
+        //•	ServiceId - integer, foreign key(required)
+        [Required]
+        public int ServiceId { get; set; }
+
+
+    }
+}
