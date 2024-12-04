d4a40d98f92eb31c5cd9231e795deb416fe26641
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2024-12-04 17:29:28 +0200

    Add files via upload

---
+++ 07 C# DB Advanced Retake Exam/C# DB Advanced Regular Exam - 03 August 2024/01. Model Definition/Shared/ValidatingConstants.cs
@@ -0,0 +1,29 @@
+﻿namespace TravelAgency.Shared
+{
+    public static class ValidatingConstants
+    {
+
+        //Customer
+        public const int CustomerNameMinLehgth = 4;
+        public const int CustomerNameMaxLehgth = 60;
+
+        public const int EmailMinLength = 6;
+        public const int EmailMaxLength = 50;
+
+      
+        public const int  PhoneNumberLegnth= 13;
+        public const string ValidPhoneNumberPattern = @"^\+\d{12}$";
+
+        public const int PackageNameMinLeghth = 2;
+        public const int PackageNameMaxLeghth = 40;
+
+        public const int DescriptionLeghth = 200;
+
+        //Guide
+
+        //•	FullName – text with length[4, 60] (required)
+        public const int GuideNameMinLehgth = 4;
+        public const int GuideNameMaxLehgth = 60;
+
+    }
+}
