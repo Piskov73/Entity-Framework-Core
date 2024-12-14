07cf93a77880a31e4610b739dd68f1e2c974fc7e
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2024-12-14 16:25:24 +0200

    Add files via upload

---
+++ 07 C# DB Advanced Retake Exam/C# DB Advanced Retake Exam - 11 December 2023/01. Model Definition/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Cadastre.Data
+{
+    public class Configuration
+    {
+        public static string ConnectionString = @"Server=localhost\SQLEXPRESS05;Database=MusicHub;Trusted_Connection=True;";
+    }
+}
---
+++ 07 C# DB Advanced Retake Exam/C# DB Advanced Retake Exam - 11 December 2023/01. Model Definition/Data/Models/Property.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cadastre.Data.Models
+{
+    public class Property
+    {
+        //        •	Id – integer, Primary Key
+        [Key]
+        public int Id { get; set; }
+
+        //•	PropertyIdentifier – text with length[16, 20] (required)
+        [Required]
+        [StringLength(20)]
+        public string PropertyIdentifier { get; set; } = null!;
+
+        //•	Area – int not negative(required)
+        [Required]
+        public int Area { get; set; }
+
+        //•	Details - text with length[5, 500] (not required)
+        [StringLength(500)]
+        public string? Details { get; set; }
+
+        //•	Address – text with length[5, 200] (required)
+        [Required]
+        [StringLength(200)]
+        public string Address { get; set; } = null!;
+
+        //•	DateOfAcquisition – DateTime(required)
+        [Required]
+        public DateTime DateOfAcquisition { get; set; }
+
+        //•	DistrictId – integer, foreign key(required)
+        [Required]
+        public int DistrictId { get; set; }
+
+        //•	District – District
+        [ForeignKey(nameof(DistrictId))]
+        public District District { get; set; } = null!;
+
+        //•	PropertiesCitizens - collection of type PropertyCitizen
+        public List<PropertyCitizen> PropertiesCitizens { get; set; } = new List<PropertyCitizen>();
+
+    }
+}
