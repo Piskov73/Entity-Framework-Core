51ca4ad27118834520d082aba601845716eb4585
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2025-01-25 08:07:21 +0200

    Delete 07 C# DB Advanced Retake Exam/C# DB Advanced Retake Exam - 11 April 2023 directory

--- 07 C# DB Advanced Retake Exam/C# DB Advanced Retake Exam - 11 April 2023/03. Data Export/Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Invoices.Data
-{
-    public static class Configuration
-    {
-        public static string ConnectionString = @"Server=localhost\SQLEXPRESS05;Database=Invoices;Integrated Security=True;Encrypt=False";
-    }
-}
